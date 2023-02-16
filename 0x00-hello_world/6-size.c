9d7a6faf46261e070583be1ff618d53438058d6d
michaelitindi <michaelitindi@gmail.com>
2023-02-16 12:28:21 -0800

    size of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ printf("Size of a char: 1 byte(s)\n");
 printf("Size of an int: 4 byte(s)\n");
 printf("Size of a long int: 4 byte(s)\n");
 printf("Size of a long long int: 8 byte(s)\n");
-printf("Size of a float:  byte(s)\n");
+printf("Size of a float:4 byte(s)\n");
 return (0);
 }
