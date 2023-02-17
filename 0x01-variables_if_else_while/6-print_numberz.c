e483252fa1e23da45a53d30b5532a53d5c60a9c5
michaelitindi <michaelitindi@gmail.com>
2023-02-17 12:58:56 -0800

    using putchar to print integer

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description: 'Printing the alphabet '
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int a = 0;
+for (a = 0; a < 10; a++)
+{
+putchar('0' + a);
+}
+putchar('\n');
+return (0);
+}
