f5de8207dc06c5bc7821fe0e48a44fc513cc2f68
michaelitindi <michaelitindi@gmail.com>
2023-02-17 12:15:53 -0800

    Printing alphabet in upper and lower case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,17 +11,17 @@
 int main(void)
 {
 char a = 'a';
-char A = 'A'; 
+char A = 'A';
 while (a <= 'z')
 {
-putchar(a); 
+putchar(a);
 a++;
 }
 while (A <= 'Z')
 {
 putchar(A);
 A++;
-} 
+}
 putchar('\n');
 return (0);
 }
