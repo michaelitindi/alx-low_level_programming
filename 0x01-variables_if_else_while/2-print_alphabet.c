ebf20df29f78002d98ae014d519fd361f0657e10
michaelitindi <michaelitindi@gmail.com>
2023-02-17 10:32:39 -0800

    printing the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description: 'checking the value of the last digit'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char a = 'a';
+while (a < 'z')
+{
+putchar(a);
+a++;
+}
+return (0);
+}
