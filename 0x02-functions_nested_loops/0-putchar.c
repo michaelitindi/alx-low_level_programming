1d953518ecbe4f5bd88dd793a4c27b29699d9215
michaelitindi <michaelitindi@gmail.com>
2023-02-21 09:00:27 -0800

    printing _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 
 /**
@@ -8,14 +9,14 @@
 
 int main(void)
 {
-_putchar(_);
-_putchar(p);
-_putchar(u);
-_putchar(t);
-_putchar(c);
-_putchar(h);
-_putchar(a);
-_putchar(r);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
