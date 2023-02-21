b546ee8de5cf0ae7eb02c81d3af18494278473b1
michaelitindi <michaelitindi@gmail.com>
2023-02-21 04:33:03 -0800

    printing putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: Program that prints putchar
+ * Return: Always (0) (Success)
+ */
+
+int main(void)
+{
+printf("_putchar");
+return (0);
+}
