06223c83c7c105af46ff0da1bd727d5a8e4b4a6c
michaelitindi <michaelitindi@gmail.com>
2023-02-21 11:36:48 -0800

    Printing the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: Functon that prints alphabet
+ * Return: Always (0) (Success)
+ */
+
+
+
+
+int main(void)
+{
+print_alphabet();
+return (0);
+}  
