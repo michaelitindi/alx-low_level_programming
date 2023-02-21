98147e8d22c16e2898c332d6bdab7a1734f4eceb
michaelitindi <michaelitindi@gmail.com>
2023-02-21 12:18:45 -0800

    added alphabet to git ignore

---
+++ 0x02-functions_nested_loops/1.main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: Program that prints putchar
+ * Return: Always (0) (Success)
+ */
+
+int main(void)
+{
+print_alphabet();
+return (0);
+}
