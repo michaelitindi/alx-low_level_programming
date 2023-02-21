c3cdceeadfc07dfaca2684066251f245cc75b30c
michaelitindi <michaelitindi@gmail.com>
2023-02-21 12:48:52 -0800

    printing alphabet ten times

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: Program that prints alphabet 10 times
+ * Return: Always (0) (Success)
+ */
+
+int main(void)
+{
+print_alphabet_x10();
+return (0);
+}
