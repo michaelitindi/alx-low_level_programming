252f55c40cca39f8fad081f5c9eb941f209d693e
michaelitindi <michaelitindi@gmail.com>
2023-02-22 08:15:57 -0800

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * @c: single letter
+ * Description: function that checks if character is alphabetic
+ * Return: void
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);   
+}  
+else if (n == 0)
+{
+_putchar(0 + '0');
+return (0);  
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+}
