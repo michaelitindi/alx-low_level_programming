92516721cc79dd428b96477abd6679c138c980fa
michaelitindi <michaelitindi@gmail.com>
2023-02-22 11:43:08 -0800

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,18 +2,18 @@
 
 /**
  * print_last_digit - Entry point
- 
+ * @n: integer
  * Description: function thatprints the last digit
  * Return: (int) Always success
  */
 
 int print_last_digit(int n)
 {
-  int m = (n % 10);
-  if (m < 0)
-    {
-      m = (m * -1);
-    }
-  _putchar(m + '0');
-  return (m);
+int m = (n % 10);
+if (m < 0)
+{
+m = (m * -1);
+}
+_putchar(m + '0');
+return (m);
 }
