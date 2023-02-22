d85e15690eec505a542cfb8f5eab360a96d0c33a
michaelitindi <michaelitindi@gmail.com>
2023-02-22 12:03:46 -0800

    adding two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ * @a,@b: two integers
+ * Description: function that checks the sign of the number
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+return(a + b);  
+}
