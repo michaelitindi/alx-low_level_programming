1a9c7a24e15ace3a1d535478e73fb1a06e1cdc99
michaelitindi <michaelitindi@gmail.com>
2023-02-23 07:39:10 -0800

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * @a: integer
+ * @b: integer
+ * Description: function that multiplies two characters
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+return(a * b);  
+}
