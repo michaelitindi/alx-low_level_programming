52a8213aa4e3ca41963587e48b5ca6ce5b15e8ba
michaelitindi <michaelitindi@gmail.com>
2023-02-23 13:09:41 -0800

    drawing lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+
+/**
+ * print_line - Entry point
+ * @n: integer
+ * Description: 'Printing line '
+ * Return: void
+ */
+
+void print_line(int n)
+{
+int a = 0;
+if (n > 0)
+{
+for (a = 0; a <= n; a++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+else
+{
+_putchar('\n');
+}
+}
