bb0110dce136590db5cb03db9f3be5d8192bf847
michaelitindi <michaelitindi@gmail.com>
2023-02-23 06:14:46 -0800

    checks if char is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * @c: single letter
+ * Description: function that checks if character is uppercase
+ * Return: void
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
