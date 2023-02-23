eafc74da3a88b5436988b5f6f469d410006ee8dd
michaelitindi <michaelitindi@gmail.com>
2023-02-23 14:26:27 -0800

    squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - print square
+ * @size: size of square in row/col units
+ */
+void print_square(int size)
+{
+int a;
+int b;
+
+if (size > 0)
+{
+for (a = 0; a < size; a++)
+{
+for (b = 0; b < size; b++)
+_putchar('#');
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
+}
