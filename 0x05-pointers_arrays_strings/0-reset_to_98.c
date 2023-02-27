731567e32bf23b206e85a9b3e047b1bea48c0b0d
michaelitindi <michaelitindi@gmail.com>
2023-02-27 07:30:11 -0800

    changing value using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ * @n: pointer variable
+ * Description: function that modifies value using pointer
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
