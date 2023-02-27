5528d8e736ed666eb3be58cd5c367bf1a7261d68
michaelitindi <michaelitindi@gmail.com>
2023-02-27 14:08:36 -0800

    print every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - Entry point
+ * @str: pointer variable
+ * Description: function that prints every other variable
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str = str + 2;
+}
+_putchar('\n');
+}
