76fdc94208c900e7659f31afde26a05a431da04a
michaelitindi <michaelitindi@gmail.com>
2023-03-02 12:24:14 -0800

    turning to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *string_toupper - Entry point
+ * @str: pointer variable
+ * Description: function that changes string to upper case
+ * Return: pointer
+ */
+
+char *string_toupper(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 32;
+}
+i++;
+}
+
+return (str);
+}
