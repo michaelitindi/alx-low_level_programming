33d97c46159c26c77798da59321440b88f95830d
michaelitindi <michaelitindi@gmail.com>
2023-03-02 13:50:34 -0800

    encoding

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,25 +2,25 @@
 
 /**
  * *leet - Entry point
- * @*: character
+ * @str: string
  * Description: function that encodes a string
- * Return: character
+ * Return: string
  */
 char *leet(char *str)
 {
 int i, j;
-char leet_chars[] = "443710";
-char leet_letters[] = "aeotlAEOTL";
+char leet_chars[] = "43071";
+char leet_letters[] = "aAeEoOtTlL";
 for (i = 0; str[i] != '\0'; i++)
 {
 for (j = 0; j < 10; j++)
 {
 if (str[i] == leet_letters[j])
 {
-str[i] = leet_chars[j/2];
+str[i] = leet_chars[j / 2];
 break;
 }
 }
 }
-return str;
+return (str);
 }
