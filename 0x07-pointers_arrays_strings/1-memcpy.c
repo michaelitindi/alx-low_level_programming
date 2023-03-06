c7d5f07ff4478220cea64b4c95c4dc83b2eede8f
michaelitindi <michaelitindi@gmail.com>
2023-03-06 11:28:30 -0800

    made changes to memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,9 +12,10 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
+char *destorg = dest;
 for (i = 0; i < n; i++)
 {
-*dest = *src;
+*dest++ = *src++;
 }
-return (dest);
+return (destorg);
 }
