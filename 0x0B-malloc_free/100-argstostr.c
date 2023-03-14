f7bec640d2fa7719022b9360004b41913249233c
michaelitindi <michaelitindi@gmail.com>
2023-03-14 15:45:38 -0700

    changes

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * argstostr - Concatenates arguments
+ * @ac: integer
+ * @av: character
+ *
+ * Return: Pointer
+ */
+
+char *argstostr(int ac, char **av)
+{
+int i, j, len;
+char *str;
+
+if (ac == 0 || av == NULL)
+{
+return (NULL);
+}
+len = 0;
+for (i = 0; i < ac; i++)
+{
+len += strlen(av[i]) + 1;
+}
+str = malloc(sizeof(char) * len);
+if (str == NULL)
+{
+return (NULL);
+}
+len = 0;
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+{
+str[len++] = av[i][j];
+}
+str[len++] = '\n';
+}
+str[len] = '\0';
+return (str);
+}
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,35 +1,45 @@
-
-
-
-
-
-
-
 #include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * alloc_grid - Returns pointer to 2d array
+ * @width: integer
+ * @height: integer
+ *
+ * Return: Pointer 
+ */
+
 
 int **alloc_grid(int width, int height)
 {
+int i;
+int j;
+int **grid;
+if (width <= 0 || height <= 0)
+{
+return (NULL);
+}
 
-    if (width <= 0 || height <= 0) {
-        return NULL;
-    }
+grid = (int **) malloc(height * sizeof(int *));
+if (grid == NULL)
+{
+return (NULL);
+}
 
+for (i = 0; i < height; i++)
+{
+grid[i] = (int *) malloc(width * sizeof(int));
+if (grid[i] == NULL)
+{
 
-    int **grid = (int **) malloc(height * sizeof(int *));
-    if (grid == NULL) {
-        return NULL;
-    }
-
-    for (int i = 0; i < height; i++) {
-        grid[i] = (int *) malloc(width * sizeof(int));
-        if (grid[i] == NULL) {
-
-            for (int j = 0; j < i; j++) {
-                free(grid[j]);
-            }
-            free(grid);
-     return (NULL);
-for (int j = 0; j < width; j++) {
+for (j = 0; j < i; j++)
+{
+free(grid[j]);
+}
+free(grid);
+return (NULL);
+for (j = 0; j < width; j++)
+{
 grid[i][j] = 0;
 }
 }
