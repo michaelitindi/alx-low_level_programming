47637aa5db5f3ed05600ee110e8b39489ad9f75e
michaelitindi <michaelitindi@gmail.com>
2023-03-14 04:36:30 -0700

    changes to create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,11 +16,11 @@ char *create_array(unsigned int size, char c)
   
   
   mal = (char*) malloc(size * sizeof(char));
-  for(i = 0; i < size; i++)
-    {
+
+
     *mal = c;
     mal++;
-    }
+
   
   
   return (mal);
