b8b04e86022fcb02e5aa1030751903ee1e37f028
michaelitindi <michaelitindi@gmail.com>
2023-03-14 08:06:09 -0700

    changed stdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,16 +16,16 @@ unsigned int i;
 unsigned int size;
 char *ptr;
 size = strlen(str);
-if (str == NULL)
+if (str == 0)
 {
-return (NULL);
+return (0);
 }
 ptr = (char *) malloc(size *sizeof(char) + 1);
 if (ptr == 0)
 {
 return (0);
 }
-for (i = 0; i < size; i++)
+for (i = 0; i < size + 1; i++)
 {
 ptr[i] = str[i];
 }
