3e45850386fb53322b30696451c0e47f1460689b
michaelitindi <michaelitindi@gmail.com>
2023-03-20 16:26:25 -0700

    create new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * init_dog - creates dog
+ * @d: struct
+ * @name: member
+ * @age: member
+ * @owner: member
+ * Return: void
+ */
+
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner) {
+
+d->name = malloc(strlen(name) + 1);
+d->owner = malloc(strlen(owner) + 1);
+strcpy(d->name, name);
+strcpy(d->owner, owner);
+d->age = age;
+}
