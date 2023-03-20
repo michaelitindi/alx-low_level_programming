0b7196b5a67d15889503194947285c737078eb0d
michaelitindi <michaelitindi@gmail.com>
2023-03-20 13:11:19 -0700

    fixed struct issue

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - a struct
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Describes characteristics
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
