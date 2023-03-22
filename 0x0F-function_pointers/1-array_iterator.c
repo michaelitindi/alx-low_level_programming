82e7f2f9dcd6f9d85bbccf1489b5e6942b220d60
michaelitindi <michaelitindi@gmail.com>
2023-03-22 10:04:04 -0700

    modified while loop

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator- iterates through array
+ * @array: array of integers
+ * @size: size of array
+ * @action: function
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+i = 0;
+if (array == 0 || size == 0 || action == NULL)
+{
+return;
+}
+
+while(i < size)
+{
+(*action)(array[i]);
+i++;
+}
+}
