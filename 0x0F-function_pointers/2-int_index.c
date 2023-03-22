9e60e9234924c7f33b904bde38f19a34db327bd6
michaelitindi <michaelitindi@gmail.com>
2023-03-22 12:28:34 -0700

    changed while loop

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - searches for integer
+ * @array: array of integers
+ * @size: size of array
+ * @cmp: function
+ * Return: integer
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+i = 0;
+if (array == 0 || cmp == NULL)
+{
+return (0);
+}
+
+if (size <= 0)
+{
+return (-1);
+}
+while (i < size)
+{
+if (cmp(array[i]) != 0)
+{
+return (i);
+}
+i++;
+}
+return (-1);
+}
