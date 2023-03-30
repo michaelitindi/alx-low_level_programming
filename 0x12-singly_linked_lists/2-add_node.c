feadc15eb801fb7f54f8c3a729910b1a32e151d2
michaelitindi <michaelitindi@gmail.com>
2023-03-30 13:28:00 -0700

    fixing betty errors

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,19 +3,28 @@
 #include <string.h>
 #include <stddef.h>
 
+/**
+ * add_node - adds nodeto linked list
+ * @head: pointer
+ * @str: character
+ * Return: node
+ */
+
+
+
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *new_node;
 new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
 {
-return (NULL); 
+return (NULL);
 }
 
 new_node->str = strdup(str);
 if (new_node->str == NULL)
 {
-free(new_node); 
+free(new_node);
 return (NULL);
 }
 new_node->len = strlen(str);
