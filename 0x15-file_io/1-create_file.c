bbcfdeffcdc66907280c911fca9629e03548f1ac
michaelitindi <michaelitindi@gmail.com>
2023-04-10 15:16:11 -0700

    create new file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: the file name
+ * @text_content: the contents of the file
+ * Return: 1 Success
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+int f, w;
+if (filename == NULL)
+{
+return (-1);
+}
+f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+if (f == -1)
+{
+return (-1);
+}
+if (text_content != NULL)
+{
+w = write(f, text_content, strlen(text_content));
+if (w == -1)
+{
+close(f);
+return (-1);
+}
+}
+close (f);
+return (1);
+}
