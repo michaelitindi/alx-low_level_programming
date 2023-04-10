dda203419bc282bb0e35d4992983b070722ef6cb
michaelitindi <michaelitindi@gmail.com>
2023-04-10 08:26:10 -0700

    fixed betty error

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,6 +12,9 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 int x;
 x = (n >> index) & 1;
-if (x != 0 && x != 1) return (-1);
+if (x != 0 && x != 1)
+{
+return (-1);
+}
 return (x);
 }
