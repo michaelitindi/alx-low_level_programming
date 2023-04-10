62b9bfe310fc22bde01593ab228c11205c8bfe4d
michaelitindi <michaelitindi@gmail.com>
2023-04-10 04:18:29 -0700

    used recursion to solve the problem

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_binary - function that prints binary representation
+ * @n: number to be printed
+ * Return: void
+ */
+
+
+
+void print_binary(unsigned long int n)
+{
+if (n > 1)
+{
+print_binary(n >> 1);
+}
+_putchar((n & 1) + '0');
+
+}
