d694ff70712a79e57d9bd4fb1586489cd7aa8834
michaelitindi <michaelitindi@gmail.com>
2023-05-18 05:00:51 -0700

    returning no of elements

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - returns number of elements
+ * @h: pointer to the head
+ * Return: number of elements
+ */
+
+
+
+
+size_t dlistint_len(const dlistint_t *h)
+{
+const dlistint_t *current = h;
+size_t i = 0;
+while(current != NULL)
+{
+current = current->next;
+i++;
+}
+return (i);
+}
