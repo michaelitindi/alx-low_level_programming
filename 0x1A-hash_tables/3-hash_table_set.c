a0521653d04db21e893df5455ea8adeeef425675
michaelitindi <michaelitindi@gmail.com>
2023-06-09 09:52:16 -0700

    adding element

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: hash table
+ * @key: key
+ * @value: value that comes with the key
+ * Return: 1 Success
+ */
+
+
+
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+unsigned long int i;
+hash_node_t *new;
+hash_node_t *current;
+if (ht == NULL || key == NULL || *key == '\0')
+{
+return (0);
+}
+i = key_index((const unsigned char *)key, ht->size);
+new = malloc(sizeof(hash_node_t));
+if (new == NULL)
+{
+return (0);
+}
+current = ht->array[i];
+while (current != NULL)
+{
+if (strcmp(current->key, key) == 0)
+{
+free(new->key);
+free(new->value);
+free(new);
+free(current->value);
+current->value = strdup(value);
+return (1);
+}
+current = current->next;
+}
+
+new->next = ht->array[i];
+ht->array[i] = new;
+
+return (1);
+}
