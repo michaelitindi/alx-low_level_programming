f60819d93f82909228f2f56fe9b7afd81cc5e1d3
michaelitindi <michaelitindi@gmail.com>
2023-06-09 10:48:47 -0700

    changed add function

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - gets value associated with a key
+ * @ht: hash table
+ * @key: key
+ *
+ * Return: value associated with the elemen
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+
+unsigned long int i;
+hash_node_t *current;
+if (ht == NULL || key == NULL || *key == '\0')
+{
+return (0);
+}
+i = key_index((const unsigned char *)key, ht->size);
+current = ht->array[i];
+while (current != NULL)
+{
+if (strcmp(current->key, key) == 0)
+{
+return current->value;
+}
+current = current->next;
+}
+return (NULL);
+}
